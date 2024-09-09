8125a95a0d728a0efcdbaf2c7d796179e6ce149d
Yelyzaveta <yelyzaveta.kashkina@gmail.com>
2024-09-09 21:57:53 +0200

    add notes

--- WAF/WAF/Home.Designer.cs
+++ WAF/WAF/Home.Designer.cs
@@ -31,13 +31,12 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
             this.picture = new System.Windows.Forms.PictureBox();
             this.listView1 = new System.Windows.Forms.ListView();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.Taskflow = new System.Windows.Forms.Label();
             this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
@@ -65,22 +64,23 @@
             this.listView1.Size = new System.Drawing.Size(325, 417);
             this.listView1.TabIndex = 1;
             this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
             // 
-            // textBox1
+            // textBox
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox1.Location = new System.Drawing.Point(18, 350);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(325, 28);
-            this.textBox1.TabIndex = 3;
+            this.textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox.Location = new System.Drawing.Point(18, 350);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(325, 28);
+            this.textBox.TabIndex = 3;
             // 
-            // richTextBox1
+            // richTextBox
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(18, 384);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(325, 144);
-            this.richTextBox1.TabIndex = 5;
-            this.richTextBox1.Text = "";
+            this.richTextBox.Location = new System.Drawing.Point(18, 384);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(325, 144);
+            this.richTextBox.TabIndex = 5;
+            this.richTextBox.Text = "";
             // 
             // button1
             // 
@@ -91,13 +91,13 @@
             this.button1.TabIndex = 6;
             this.button1.Text = "Save";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.IndianRed;
             this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(0, 590);
             this.panel1.Name = "panel1";
@@ -108,7 +108,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label4.Location = new System.Drawing.Point(56, 25);
+            this.label4.Location = new System.Drawing.Point(111, 25);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(57, 22);
             this.label4.TabIndex = 3;
@@ -119,27 +119,17 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label3.Location = new System.Drawing.Point(264, 25);
+            this.label3.Location = new System.Drawing.Point(347, 25);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(83, 22);
             this.label3.TabIndex = 2;
             this.label3.Text = "Calendar";
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label2.Location = new System.Drawing.Point(442, 25);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(75, 22);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Settings";
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(654, 25);
+            this.label1.Location = new System.Drawing.Point(590, 25);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(70, 22);
             this.label1.TabIndex = 0;
@@ -172,9 +162,9 @@
             this.Controls.Add(this.Taskflow);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.richTextBox);
             this.Controls.Add(this.monthCalendar1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.picture);
             this.Name = "Home";
@@ -191,13 +181,12 @@
 
         private System.Windows.Forms.PictureBox picture;
         private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.RichTextBox richTextBox;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label Taskflow;
         private System.Windows.Forms.MonthCalendar monthCalendar1;
--- WAF/WAF/Home.cs
+++ WAF/WAF/Home.cs
@@ -4,11 +4,13 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace WAF
 {
@@ -36,7 +38,45 @@ namespace WAF
         private void label1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Log out!");
-            this.Close();
+            this.Hide();
+            Form1 f = new Form1();
+            f.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Calendar = monthCalendar1.SelectionRange.Start.ToString("yyyy-MM-dd");
+            string Titleofnote = textBox.Text;
+            string Note = richTextBox.Text;
+            string filenote = "notes.txt";
+
+            if (string.IsNullOrEmpty(Note))
+            {
+                MessageBox.Show("Write something...");
+            }
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(filenote, true)) // true - otwieramy plik w trybie dopisywania
+                {
+                    writer.WriteLine($"{Calendar};{Titleofnote};{Note}");
+                }
+                MessageBox.Show("User data saved successfully!");
+
+                richTextBox.Clear();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error while saving data: " + ex.Message);
+            }
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
-}
+    }
+
